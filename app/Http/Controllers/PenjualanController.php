1cb29f4f34baab8906f92581b9c4fc72793fde1f
amircjr01 <amircjr01@gmail.com>
2024-09-20 11:21:03 +0700

    Penambahan Projek UAS

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -3,26 +3,86 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PenjualanBarang;
+use App\Models\User;
 
-use App\Models\Penjualan;
 
-class PenjualanController extends Controller
+class PenjualanBarangController extends Controller
 {
-    public function index(){
-        $penjualan = Penjualan::all();
-        return view('index',['penjualan'=>$penjualan]);
+    public function index(Request $request)
+    {
+        $penjualans = PenjualanBarang::all();
+        $user = $request->session()->get('user');
+        $user = User::where('id', $user->id)->first();
+        return view('penjualan.index', compact('penjualans', 'user'));
     }
 
-    public function add(Request $req){
-        $penjualan = new Penjualan;
-        $penjualan->id = $req->id;
-        $penjualan->nama_barang = $req->nama_barang;
-        $penjualan->tanggal_penjualan = $req->tanggal_penjualan;
-        $penjualan->pelanggan = $req->pelanggan;
-        $penjualan->jumlah = $req->jumlah;
-        $penjualan->harga_satuan = $req->harga_satuan;
-        $penjualan->total = $penjualan->jumlah * $penjualan->harga_satuan;
-        $penjualan->save();
-        return redirect()->back(); 
+    public function create(Request $request)
+    {
+        $user = $request->session()->get('user');
+        $user = User::where('id', $user->id)->first();
+        return view('penjualan.create', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nama_barang' => 'required|max:50',
+            'tanggal_penjualan' => 'required|date',
+            'pelanggan' => 'required|max:50',
+            'jumlah' => 'required|numeric',
+            'harga_satuan' => 'required|numeric',
+        ]);
+
+        PenjualanBarang::create([
+            'nama_barang' => $request->nama_barang,
+            'tanggal_penjualan' => $request->tanggal_penjualan,
+            'pelanggan' => $request->pelanggan,
+            'jumlah' => $request->jumlah,
+            'harga_satuan' => $request->harga_satuan,
+            'total' => $request->jumlah * $request->harga_satuan,
+        ]);
+
+        session()->flash('success', 'Data penjualan baru berhasil ditambahkan!');
+        return redirect()->route('list-penjualan');
+    }
+
+    public function edit(Request $request, PenjualanBarang $penjualanBarang)
+    {
+        $user = $request->session()->get('user');
+        $user = User::where('id', $user->id)->first();
+        return view('penjualan.edit', compact('penjualanBarang', 'user'));
+    }
+
+    public function update(Request $request, PenjualanBarang $penjualanBarang)
+    {
+        $validated = $request->validate([
+            'nama_barang' => 'required|max:50',
+            'tanggal_penjualan' => 'required|date',
+            'pelanggan' => 'required|max:50',
+            'jumlah' => 'required|numeric',
+            'harga_satuan' => 'required|numeric',
+        ]);
+
+        $penjualanBarang->update([
+            'nama_barang' => $request->nama_barang,
+            'tanggal_penjualan' => $request->tanggal_penjualan,
+            'pelanggan' => $request->pelanggan,
+            'jumlah' => $request->jumlah,
+            'harga_satuan' => $request->harga_satuan,
+            'total' => $request->jumlah * $request->harga_satuan,
+        ]);
+        
+        session()->flash('success', 'Data penjualan baru berhasil diperbaharui!');
+
+        return redirect()->route('list-penjualan');
+    }
+
+    public function delete(PenjualanBarang $penjualanBarang)
+    {
+        $penjualanBarang->delete();
+        session()->flash('success', 'Data penjualan baru berhasil dihapus!');
+
+        return redirect()->route('list-penjualan');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FileUploadController;
+use App\Http\Middleware\Authenticate;
 
-Route::get('/', [PenjualanController::class, 'index']);
-Route::post('/add', [PenjualanController::class, 'add']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login-form');
+Route::post('/login', [LoginController::class, 'login'])->name('login-post');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/', [DashboardController::class, 'index'])->name('welcome-page')->middleware(Authenticate::class);
+Route::get('/penjualan', [PenjualanController::class, 'index'])->name("list-penjualan")->middleware(Authenticate::class);
+Route::get('/penjualan/create', [PenjualangController::class, 'create'])->name("create-penjualan")->middleware(Authenticate::class);
+Route::post('/penjualan/store', [PenjualanController::class, 'store'])->name("post-penjualan")->middleware(Authenticate::class);
+Route::get('/penjualan/{penjualan}/edit', [PenjualanController::class, 'edit'])->name("edit-penjualan")->middleware(Authenticate::class);
+Route::put('/penjualan/{penjualan}/update', [PenjualanController::class, 'update'])->name("update-penjualan")->middleware(Authenticate::class);
+Route::delete('/penjualan/{penjualan}/delete', [PenjualanController::class, 'delete'])->name("delete-penjualan")->middleware(Authenticate::class);
+Route::get('/profile', [FileUploadController::class, 'FormAvatar'])->name('profile')->middleware(Authenticate::class);
+Route::put('/profile/avatar/{user}', [FileUploadController::class, 'store'])->name('profile-store')->middleware(Authenticate::class);
